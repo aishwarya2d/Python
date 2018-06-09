40dfa243489fa6f7f878f997b2c7d0efa4ce6e8f
Aishwarya Dhobale <37836087+aishwarya2d@users.noreply.github.com>
2018-06-09 23:09:52 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,6 @@
+a = 10
+b = 5
+print("The addition of two number:",a+b)
+print("The subraction of two number:",a-b)
+print("The multipplication of two number:",a*b)
+print("The division of two number:",a/b)
---
+++ prime.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a number"))
+if n > 1:
+	for i in range (2,n):
+		if n %2==0:
+		  print("The number is not a prime number")
+		else:
+  		  print("The number is prime number")
+else:
+   print("The number is not a prime number")
